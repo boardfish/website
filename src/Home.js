9f6d6b0b5b33db73ca5c5b81560a0ec468956800
Fl_GUI <flor.guilini@hotmail.com>
2021-06-08 00:08:39 +0200

    semantic ui makes things pretty for free

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,16 @@
 import banner from "./images/fastbun.png";
+import { Container, Header, Image } from 'semantic-ui-react';
 
 
 function Home() {
-  return <>
-    <p> Formula bun </p>
-    <p> gaming </p>
-    <img src={banner}/>
-    <p>
-      <a href={`srb2kart://ip/${process.env.REACT_APP_SERVER}`}> join formula bun </a>
-    </p>
-    </>
+  return <Container>
+    <Header as="h1"> Formula bun </Header>
+      <Header> gaming </Header>
+      <Image src={banner}/>
+      <p>
+        <a href={`srb2kart://ip/${process.env.REACT_APP_SERVER}`}> join formula bun </a>
+      </p>
+    </Container>
 }
 
 export default Home;
